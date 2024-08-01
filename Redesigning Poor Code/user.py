6af5317440ae86e6623e980753e99ccce36f4a70
Vadigi Hemanth Kumar <114655387+vadigihemanthkumar@users.noreply.github.com>
2024-08-01 11:06:45 +0530

    Update user.py

--- Redesigning Poor Code/user.py
+++ Redesigning Poor Code/user.py
@@ -1,9 +1,15 @@
-# user.py
+from datetime import datetime
 
 class User:
     def __init__(self, name, user_id):
         self.name = name
         self.user_id = user_id
+        self.added_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
     def __str__(self):
-        return f"Name: {self.name}, User ID: {self.user_id}"
+        return f"Name: {self.name}, User ID: {self.user_id}, Added: {self.added_date}"
+
+    def update(self, name=None):
+        if name:
+            self.name = name
+
