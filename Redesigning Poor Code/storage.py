3b39486414af67dfae6ac79abc457ae37dabce86
Vadigi Hemanth Kumar <114655387+vadigihemanthkumar@users.noreply.github.com>
2024-08-01 11:34:24 +0530

    Update storage.py

--- Redesigning Poor Code/storage.py
+++ Redesigning Poor Code/storage.py
@@ -1,5 +1,6 @@
+# storage.py
+
 import json
-import os
 
 class Storage:
     @staticmethod
@@ -9,21 +10,12 @@ class Storage:
 
     @staticmethod
     def load_from_file(filename):
-        if not os.path.isfile(filename):
-            print(f"Error: File '{filename}' not found.")
-            # Create default file content based on the filename
-            default_data = [] if filename != 'checkouts.json' else [{}]
-            Storage.save_to_file(default_data, filename)
-            return default_data
         try:
             with open(filename, 'r') as file:
                 return json.load(file)
-        except json.JSONDecodeError as e:
-            print(f"Error decoding JSON from file '{filename}': {e}")
+        except FileNotFoundError:
             return []
-        except Exception as e:
-            print(f"Unexpected error: {e}")
-            return []
+
 
 
 
