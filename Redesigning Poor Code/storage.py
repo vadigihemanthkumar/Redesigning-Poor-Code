9c7409263259ff1b35b5d2c2c99984dcf25be364
Vadigi Hemanth Kumar <114655387+vadigihemanthkumar@users.noreply.github.com>
2024-08-01 12:22:54 +0530

    Update storage.py

--- Redesigning Poor Code/storage.py
+++ Redesigning Poor Code/storage.py
@@ -1,19 +1,20 @@
-# storage.py
-
 import json
 
 class Storage:
     @staticmethod
     def save_to_file(data, filename):
+        # Save data to a JSON file
         with open(filename, 'w') as file:
             json.dump(data, file, indent=4)
 
     @staticmethod
     def load_from_file(filename):
         try:
+            # Try to load data from a JSON file
             with open(filename, 'r') as file:
                 return json.load(file)
         except FileNotFoundError:
+            # If the file doesn't exist, return an empty list
             return []
 
 
