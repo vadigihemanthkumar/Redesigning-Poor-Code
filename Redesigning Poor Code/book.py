6d0dbc1d524d175cbbb036bcf0aa00a8a9e2e68d
Vadigi Hemanth Kumar <114655387+vadigihemanthkumar@users.noreply.github.com>
2024-08-01 12:32:52 +0530

    Update book.py

--- Redesigning Poor Code/book.py
+++ Redesigning Poor Code/book.py
@@ -3,6 +3,7 @@ from datetime import datetime
 class Book:
     def __init__(self, title, author, isbn, is_checked_out=False, added_date=None, 
                  checkout_date=None, checkin_date=None):
+        # Initialize a new book with given details
         self.title = title
         self.author = author
         self.isbn = isbn
@@ -12,13 +13,14 @@ class Book:
         self.checkin_date = checkin_date
 
     def __str__(self):
+        # Return a string representation of the book details
         return (f"Title: {self.title}, Author: {self.author}, ISBN: {self.isbn}, "
                 f"Available: {not self.is_checked_out}, Added: {self.added_date}, "
                 f"Checkout Date: {self.checkout_date}, Checkin Date: {self.checkin_date}")
 
     def update(self, title=None, author=None):
+        # Update book title or author if new values are provided
         if title:
             self.title = title
         if author:
             self.author = author
-
